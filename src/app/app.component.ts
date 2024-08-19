4284c6cd225b9ba6a26a41c5fd58bbfa6c43ca39
Ali Haider <ha4219466@gmail.com>
2024-08-20 03:05:03 +0500

    algo visualizer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { BubbleSortVisualizerComponent } from './component/bubble-sort-visualizer/bubble-sort-visualizer.component';
+import { InsertionSortVisualizerComponent } from './component/insertion-sort-visualizer/insertion-sort-visualizer.component';
+import { NavbarComponent } from './component/shared/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    BubbleSortVisualizerComponent,
+    InsertionSortVisualizerComponent,
+    NavbarComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'algorithm-visualizer';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { BubbleSortVisualizerComponent } from './component/bubble-sort-visualizer/bubble-sort-visualizer.component';
+import { InsertionSortVisualizerComponent } from './component/insertion-sort-visualizer/insertion-sort-visualizer.component';
+import { SelectSortVisualizerComponent } from './component/select-sort-visualizer/select-sort-visualizer.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'bubble-sort-visualizer',
+    component: BubbleSortVisualizerComponent,
+  },
+  {
+    path: 'insertion-sort-visualizer',
+    component: InsertionSortVisualizerComponent,
+  },
+  {
+    path: 'select-sort-visualizer',
+    component: SelectSortVisualizerComponent,
+  },
+];
