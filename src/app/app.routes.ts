bfa5015e9c7146b5ade0d24d27a02f14d324ff95
Ali Haider <ha4219466@gmail.com>
2024-08-21 03:07:11 +0500

    update routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { BubbleSortVisualizerComponent } from './component/bubble-sort-visualizer/bubble-sort-visualizer.component';
 import { InsertionSortVisualizerComponent } from './component/insertion-sort-visualizer/insertion-sort-visualizer.component';
 import { SelectSortVisualizerComponent } from './component/select-sort-visualizer/select-sort-visualizer.component';
+import { MergeSortVisualizerComponent } from './component/merge-sort-visualizer/merge-sort-visualizer.component';
+import { LinearSearchVisualizerComponent } from './component/linear-search-visualizer/linear-search-visualizer.component';
 
 export const routes: Routes = [
   {
@@ -15,5 +17,13 @@ export const routes: Routes = [
   {
     path: 'select-sort-visualizer',
     component: SelectSortVisualizerComponent,
+  },
+  {
+    path: 'merge-sort-visualizer',
+    component: MergeSortVisualizerComponent,
+  },
+  {
+    path: 'linear-search-visualizer',
+    component: LinearSearchVisualizerComponent,
   },
 ];
