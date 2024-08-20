70f57730e567467d474bbc7b6c30908075339117
Ali Haider <ha4219466@gmail.com>
2024-08-21 03:07:02 +0500

    update navbar

--- src/app/component/shared/navbar/navbar.component.ts
+++ src/app/component/shared/navbar/navbar.component.ts
@@ -1,11 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterOutlet, RouterModule],
+  imports: [RouterOutlet, RouterModule, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  dropdownStates = {
+    sort: false,
+    search: false,
+    // Add more dropdowns here as needed
+  };
+  toggleDropdown(type: keyof typeof this.dropdownStates) {
+    this.dropdownStates[type] = !this.dropdownStates[type];
+
+    for (const key in this.dropdownStates) {
+      if (key !== type) {
+        this.dropdownStates[key as keyof typeof this.dropdownStates] = false;
+      }
+    }
+  }
+}
