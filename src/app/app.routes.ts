905d78fe88a6c262014461163849b416249f2e6f
Ali Haider <ha4219466@gmail.com>
2024-11-30 14:00:59 +0500

    lazy loading in routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,68 +1,95 @@
 import { Routes } from '@angular/router';
-import { BubbleSortVisualizerComponent } from './component/Sort_Algo/bubble-sort-visualizer/bubble-sort-visualizer.component';
-import { HeapSortVisualizerComponent } from './component/Sort_Algo/heap-sort-visualizer/heap-sort-visualizer.component';
-import { InsertionSortVisualizerComponent } from './component/Sort_Algo/insertion-sort-visualizer/insertion-sort-visualizer.component';
-import { MergeSortVisualizerComponent } from './component/Sort_Algo/merge-sort-visualizer/merge-sort-visualizer.component';
-import { QuickSortVisualizerComponent } from './component/Sort_Algo/quick-sort-visualizer/quick-sort-visualizer.component';
-import { SelectSortVisualizerComponent } from './component/Sort_Algo/select-sort-visualizer/select-sort-visualizer.component';
-import { BinarySearchVisualizerComponent } from './component/Search_Algo/binary-search-visualizer/binary-search-visualizer.component';
-import { LinearSearchVisualizerComponent } from './component/Search_Algo/linear-search-visualizer/linear-search-visualizer.component';
-import { PreorderTraversalComponent } from './component/Tree_Algo/preorder-traversal/preorder-traversal.component';
-import { PostOrderTraversalComponent } from './component/Tree_Algo/post-order-traversal/post-order-traversal.component';
-import { InOrderTraversalComponent } from './component/Tree_Algo/in-order-traversal/in-order-traversal.component';
-import { CountingSortComponent } from './component/Sort_Algo/counting-sort/counting-sort.component';
 
 export const routes: Routes = [
   {
     path: '',
-    component: BubbleSortVisualizerComponent,
+    loadComponent: () =>
+      import(
+        './component/Sort_Algo/bubble-sort-visualizer/bubble-sort-visualizer.component'
+      ).then((m) => m.BubbleSortVisualizerComponent),
   },
   {
     path: 'bubble-sort-visualizer',
-    component: BubbleSortVisualizerComponent,
+    loadComponent: () =>
+      import(
+        './component/Sort_Algo/bubble-sort-visualizer/bubble-sort-visualizer.component'
+      ).then((m) => m.BubbleSortVisualizerComponent),
   },
   {
     path: 'insertion-sort-visualizer',
-    component: InsertionSortVisualizerComponent,
+    loadComponent: () =>
+      import(
+        './component/Sort_Algo/insertion-sort-visualizer/insertion-sort-visualizer.component'
+      ).then((m) => m.InsertionSortVisualizerComponent),
   },
   {
     path: 'select-sort-visualizer',
-    component: SelectSortVisualizerComponent,
+    loadComponent: () =>
+      import(
+        './component/Sort_Algo/select-sort-visualizer/select-sort-visualizer.component'
+      ).then((m) => m.SelectSortVisualizerComponent),
   },
   {
     path: 'merge-sort-visualizer',
-    component: MergeSortVisualizerComponent,
+    loadComponent: () =>
+      import(
+        './component/Sort_Algo/merge-sort-visualizer/merge-sort-visualizer.component'
+      ).then((m) => m.MergeSortVisualizerComponent),
   },
   {
     path: 'quick-sort-visualizer',
-    component: QuickSortVisualizerComponent,
+    loadComponent: () =>
+      import(
+        './component/Sort_Algo/quick-sort-visualizer/quick-sort-visualizer.component'
+      ).then((m) => m.QuickSortVisualizerComponent),
   },
   {
     path: 'heap-sort-visualizer',
-    component: HeapSortVisualizerComponent,
+    loadComponent: () =>
+      import(
+        './component/Sort_Algo/heap-sort-visualizer/heap-sort-visualizer.component'
+      ).then((m) => m.HeapSortVisualizerComponent),
   },
   {
     path: 'linear-search-visualizer',
-    component: LinearSearchVisualizerComponent,
+    loadComponent: () =>
+      import(
+        './component/Search_Algo/linear-search-visualizer/linear-search-visualizer.component'
+      ).then((m) => m.LinearSearchVisualizerComponent),
   },
   {
     path: 'binary-search-visualizer',
-    component: BinarySearchVisualizerComponent,
+    loadComponent: () =>
+      import(
+        './component/Search_Algo/binary-search-visualizer/binary-search-visualizer.component'
+      ).then((m) => m.BinarySearchVisualizerComponent),
   },
   {
     path: 'preorder-traversal',
-    component: PreorderTraversalComponent,
+    loadComponent: () =>
+      import(
+        './component/Tree_Algo/preorder-traversal/preorder-traversal.component'
+      ).then((m) => m.PreorderTraversalComponent),
   },
   {
     path: 'postorder-traversal',
-    component: PostOrderTraversalComponent,
+    loadComponent: () =>
+      import(
+        './component/Tree_Algo/post-order-traversal/post-order-traversal.component'
+      ).then((m) => m.PostOrderTraversalComponent),
   },
   {
     path: 'inorder-traversal',
-    component: InOrderTraversalComponent,
+    loadComponent: () =>
+      import(
+        './component/Tree_Algo/in-order-traversal/in-order-traversal.component'
+      ).then((m) => m.InOrderTraversalComponent),
   },
   {
     path: 'counting-sort-visualizer',
-    component: CountingSortComponent,
+    loadComponent: () =>
+      import(
+        './component/Sort_Algo/counting-sort/counting-sort.component'
+      ).then((m) => m.CountingSortComponent),
   },
 ];
